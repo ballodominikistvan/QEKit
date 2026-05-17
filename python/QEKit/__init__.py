7ba736028d720db12e3a9104bf031d0b030b4334
Bálló Dominik István <ballodominikistvan@gmail.com>
2026-05-17 15:58:10 +0200

    Initial commit

---
+++ python/QEKit/__init__.py
@@ -0,0 +1,6 @@
+from .QEKit import *
+
+
+__doc__ = QEKit.__doc__
+if hasattr(QEKit, "__all__"):
+    __all__ = QEKit.__all__
---
+++ python/tests/test_all.py
@@ -0,0 +1,6 @@
+import pytest
+import QEKit
+
+
+def test_sum_as_string():
+    assert QEKit.sum_as_string(1, 1) == "2"
